047dcd3ef9f67b575057e028db835bec6538cb04
caina <cainazuma@yahoo.com>
2023-11-06 02:18:09 -0300

    commit

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function carregar() {
 
   if (hora >= 5 && hora < 12) {
     // Bom dia
-    img.src = "manha_Circle.png";
+    img.src = "imagens/manha_Circle.png";
     document.body.style.background = "#f4d181";
   } else if (hora >= 12 && hora < 18) {
     // Boa tarde
-    img.src = "tarde_Circle.png";
+    img.src = "imagens/tarde_Circle.png";
     document.body.style.background = "#db9b61";
   } else {
     // Boa noite
-    img.src = "noite_Circle.png";
+    img.src = "imagens/noite_Circle.png";
     document.body.style.background = "#14849c";
   }
 }
